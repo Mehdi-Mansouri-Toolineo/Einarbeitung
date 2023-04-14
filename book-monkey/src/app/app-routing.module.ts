08ecb7a2fdcec11ecfb041da679d6c01f539b034
Mehdi-Mansouri-Toolineo <mehdi.mansouri@toolineo.de>
2023-04-14 14:12:54 +0200

    created Home Components

--- book-monkey/src/app/app-routing.module.ts
+++ book-monkey/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookDetailsComponent } from './books/book-details/book-details.component';
 
 const routes: Routes = [];
 
