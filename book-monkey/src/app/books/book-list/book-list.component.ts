81468f0a3d7c1ab475916b820617a8309690c280
Mehdi-Mansouri-Toolineo <mehdi.mansouri@toolineo.de>
2023-04-14 08:18:53 +0200

    add book service

--- book-monkey/src/app/books/book-list/book-list.component.ts
+++ book-monkey/src/app/books/book-list/book-list.component.ts
@@ -1,56 +1,19 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnChanges,
-  OnInit,
-  Output,
-} from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Book } from '../../shared/book';
+import { BookStoreService } from 'src/app/shared/book-store.service';
 
 @Component({
   selector: 'bm-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css'],
 })
-export class BookListComponent implements OnInit, OnChanges {
-  public books: Book[] = [];
+export class BookListComponent {
+  books: Book[] = [];
   @Output() selectBook = new EventEmitter<Book>();
-
-  @Input() foo?: string;
-  constructor() {
-    this.books = [
-      {
-        isbn: '12345',
-        title: 'Tierisch gut kochen',
-        authors: ['Mrs Chimp', 'Mr Gorilla'],
-        published: '2022-06-20',
-        subtitle: 'Rezepte von Affe bis Zebra',
-        thumbnailUrl: 'https://cdn.ng-buch.de/kochen.png',
-        description: 'Immer lecker und gut',
-      },
-      {
-        isbn: '67890',
-        title: 'Backen mit Affen',
-        authors: ['Orang Utan'],
-        published: '2022-07-15',
-        subtitle: 'Bananenbrot und mehr',
-        thumbnailUrl: 'https://cdn.ng-buch.de/backen.png',
-        description: 'Tolle Backtipps für Mensch und Tier',
-      },
-    ];
+  constructor(private service: BookStoreService) {
+    this.books = this.service.getAll();
   }
   doSelect(book: Book) {
     this.selectBook.emit(book);
-  }
-
-  ngOnChanges(): void {
-    this.foo = 'ngOnChanges';
-    console.log('');
-  }
-
-  ngOnInit(): void {
-    this.foo = 'Hello';
-    console.log('');
   }
 }
