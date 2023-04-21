1f0bce7ecd47fc1cf7574dde7c9f0d9ea02be598
Mehdi-Mansouri-Toolineo <mehdi.mansouri@toolineo.de>
2023-04-21 14:46:24 +0200

    add ngform and setting to Create Book

--- book-monkey/src/app/admin/admin.module.ts
+++ book-monkey/src/app/admin/admin.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LocalIsoDateValueAccessor } from 'angular-date-value-accessor';
 import { BookFormComponent } from './book-form/book-form.component';
 import { BookCreateComponent } from './book-create/book-create.component';
 
 @NgModule({
   declarations: [BookFormComponent, BookCreateComponent],
-  imports: [CommonModule, AdminRoutingModule, FormsModule],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    ReactiveFormsModule,
+    LocalIsoDateValueAccessor,
+  ],
 })
 export class AdminModule {}
--- book-monkey/src/app/admin/book-form/book-form.component.ts
+++ book-monkey/src/app/admin/book-form/book-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Book } from 'src/app/shared/book';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'bm-book-form',
@@ -7,10 +8,35 @@ import { Book } from 'src/app/shared/book';
   styleUrls: ['./book-form.component.css'],
 })
 export class BookFormComponent {
-  book: Book = { isbn: '', title: '', authors: [''] };
-
   @Output() submitBook = new EventEmitter<Book>();
+  form = new FormGroup({
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    subtitle: new FormControl('', { nonNullable: true }),
+    isbn: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(13),
+      ],
+    }),
+    description: new FormControl('', { nonNullable: true }),
+    published: new FormControl('', { nonNullable: true }),
+    thumbnailUrl: new FormControl('', { nonNullable: true }),
+    authors: new FormArray([new FormControl('', { nonNullable: true })]),
+  });
   submitForm() {
-    this.submitBook.emit(this.book);
+    const formValue = this.form.getRawValue();
+    const newBook: Book = {
+      ...formValue,
+      authors: [], // TODO: echte Eingaben
+    };
+    this.submitBook.emit(newBook);
+  }
+  get authors() {
+    return this.form.controls.authors;
   }
 }
